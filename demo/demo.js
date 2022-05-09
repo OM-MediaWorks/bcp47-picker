5a75de3b1b8774740903fb5a1e395508062149c6
Daniel Beeke <mail@danielbeeke.nl>
2022-05-09 15:06:56 +0200

    build via parcel

---
+++ demo/demo.js
@@ -0,0 +1,10 @@
+import { register } from '../dist/index.cjs'
+
+register()
+
+const picker = document.createElement('bcp47-picker')
+document.body.appendChild(picker)
+
+picker.addEventListener('updated', (event) => {
+  console.log(event)
+})
