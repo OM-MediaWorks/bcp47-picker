f22471e4105c00d2768688c61d696a4b3bd3a395
Daniel Beeke <mail@danielbeeke.nl>
2022-05-09 14:18:04 +0200

    up

--- docs/demo.js
+++ docs/demo.js
@@ -3,9 +3,6 @@ import { register } from './index.cjs'
 register()
 
 const picker = document.createElement('bcp47-picker')
-/** @ts-ignore */
-// picker.value = 'de-AT'
-
 document.body.appendChild(picker)
 
 picker.addEventListener('updated', (event) => {
